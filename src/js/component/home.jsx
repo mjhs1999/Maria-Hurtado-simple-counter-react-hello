b9e07d05571daab708b4ae03b782e131ffefcb1c
mjhs1999 <138990797+mjhs1999@users.noreply.github.com>
2023-12-11 20:04:34 +0000

    Counter is made so it can have  different digits at the same time

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,106 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import SimpleCounter from "./simplecounter";
+import Functionality from "./functionality";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
+
+	const [counter, setCounter] = useState (0);
+	const [countDown, setCountDown] = useState (60);
+	const [isActive,setIsActive] = useState(false);
+	
+
+	const handler = value => setIsActive (value)
+
+
+	
+	let timer;
+	
+	const handleStart = () => {
+	  setCounter(counter + 1);
+	   setIsActive(true)
+	};
+  
+	const handleStop = () => {
+		setIsActive (false);
+	};
+	const handlereset = () => {
+	  setCounter(0);
+	  clearTimeout(timer);
+	};
+	const handleDown = () => {
+		setIsActive(false);
+		
+		setCounter(setCountDown (countDown -1), 1000); 
+		return () => clearInterval (timer);
+	  };
+	
+	useEffect(() => {
+		let intervalId;
+	
+		if (isActive) {
+		  intervalId = setInterval(() => {
+			setCounter((prevCounter) => prevCounter + 1);
+		  }, 1000);
+		} else {
+		  clearInterval(intervalId);
+		}
+	
+		if (counter === countDown) {
+		  window.alert("Target time reached!");
+		}
+	
+		return () => {
+		  clearInterval(intervalId);
+		};
+	  }, [isActive , counter, countDown]);
+	  
+	  
+	
+
+
+	  
+
+	function calculateSeconds (secondCounter, placeValue) {
+		return Math.floor(secondCounter / placeValue) % 10
+	}
+	
+	 
+
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+		<SimpleCounter 
+		oneDigit = {calculateSeconds(counter, 100000)}
+		twoDigit = {calculateSeconds(counter, 10000)}
+		threeDigit = {calculateSeconds(counter, 1000)}
+		fourDigit = {calculateSeconds(counter, 100)}
+		fiveDigit = {calculateSeconds(counter, 10)}
+		sixDigit = {calculateSeconds(counter, 1)} />
+		 <div className="text-center mt-1">
+          <div className="btn-group" role="group">
+            <button className="btn btn-primary" onClick={handleStart} disabled={isActive }>
+             Start
+            </button>
+            <button className="btn btn-secondary" onClick={handleStop} disabled={!isActive } >
+              Stop
+            </button>
+            <button className="btn btn-danger" onClick={handlereset} >
+              Reset
+            </button>
+			<button className="btn btn-danger" onClick={handleDown} >
+              Countdown
+            </button>
+          </div>
+        </div>
+
+		</>
 	);
 };
 
