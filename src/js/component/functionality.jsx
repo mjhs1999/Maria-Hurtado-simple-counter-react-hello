f76d3236d3651ff4d80bdbfa71d4b461ac0200d3
mjhs1999 <138990797+mjhs1999@users.noreply.github.com>
2023-12-11 20:06:23 +0000

    Need to understand why my countdown function is not working, this is extra...

---
+++ src/js/component/functionality.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect} from "react";
+
+const Functionality = () => {
+    
+	
+  
+    return (
+      <>
+        
+      </>
+    );
+  };
+
+
+export default Functionality;
